de44ddb9b9aaf6dd691a7c91736bb84fe291e729
SSylvain1989 <sylvain.sigonnez@gmail.com>
2021-06-09 17:22:10 +0200

    feat: we can display all post content

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,6 +1,10 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import { blogPosts } from '../../lib/data'
 
-export default function BlogPage({title, date, content}) {
+// here we have the return object 'props' of 'getStaticProps' fonction .
+// Two solution , declare this props like (props) or destructure ({ title, content, date })
+// to make it easier to use , we destrure the props object directly
+export default function BlogPage({ title, content, date }) {
   return (
     <div>
       <Head>
@@ -12,32 +16,18 @@ export default function BlogPage({title, date, content}) {
         <h1>
           {title}
         </h1>
+        <p>{content}</p>
       </main>
     </div>
   );
 }
 
 export async function getStaticProps(context) {
+  console.log('hi', context);
+  const { params } = context;
   return {
-    props: {}, // will be passed to the page component as props
-  }
-}
-
-// This function gets called at build time on server-side.
-// It may be called again, on a serverless function, if
-// revalidation is enabled and a new request comes in
-export async function getStaticProps() {
-  const res = await fetch('https://.../posts')
-  const posts = await res.json()
-
-  return {
-    props: {
-      posts,
-    },
-    // Next.js will attempt to re-generate the page:
-    // - When a request comes in
-    // - At most once every 10 seconds
-    revalidate: 10, // In seconds
+    // i will find the post that match with the slug , with the find methode and will return all props of the post ( title, slug, date , content ...)
+    props: blogPosts.find((post) => post.slug === params.slug), // will be passed to the page component as props
   }
 }
 
@@ -45,18 +35,65 @@ export async function getStaticProps() {
 // It may be called again, on a serverless function, if
 // the path has not been generated.
 export async function getStaticPaths() {
-  
-  const res = await fetch('https://.../posts')
-  const posts = await res.json()
+  // Get the paths we want to pre-render based on posts
+  return {
+    paths: blogPosts.map((post) => ({
+      params: {
+        slug: post.slug,
+      },
+    })),
+    fallback: false,
+  };
+}
+
+// This function gets called at build time on server-side.
+// It may be called again, on a serverless function, if
+// revalidation is enabled and a new request comes in
+// export async function getStaticProps() {
+//   const res = await fetch('https://.../posts')
+//   const posts = await res.json()
+
+//   return {
+//     props: {
+//       posts,
+//     },
+//     // Next.js will attempt to re-generate the page:
+//     // - When a request comes in
+//     // - At most once every 10 seconds
+//     revalidate: 10, // In seconds
+//   }
+// }
+
+
+// ***** 
+  // const foo = {
+  //   paths: blogPosts.map((post) => ({
+  //     params: {
+  //       slug: post.slug,
+  //     },
+  //   })),
+  //   fallback: false,
+  // };
+  // console.log(JSON.stringify(foo, null, ' ')); // it will return { paths:
+  //   [ { params: [Object] },
+  //     { params: [Object] },
+  //     { params: [Object] } ],
+  //  fallback: false }
+  // because the object is to long / to deeply nested the console log will return this [object].
+  // solution :   console.log(JSON.stringify(foo)); or for a " pretty render "   console.log(JSON.stringify(foo, null, ' '));
+  // output : {"paths":[{"params":{"slug":"first"}},{"params":{"slug":"second"}},{"params":{"slug":"thirds"}}],"fallback":false}
+  // return foo;
+// }
+  // const res = await fetch('https://.../posts')
+  // const posts = await res.json()
 
   // Get the paths we want to pre-render based on posts
-  const paths = posts.map((post) => ({
-    params: { id: post.id },
-  }))
+  // const paths = posts.map((post) => ({
+  //   params: {
+  //     slug: post.slug,
+  //   },
+  // }))
 
   // We'll pre-render only these paths at build time.
   // { fallback: blocking } will server-render pages
   // on-demand if the path doesn't exist.
-  return { paths, fallback: 'blocking' }
-}
-
