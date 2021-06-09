b59bdb051ddc92192e82c38d1498952f66f394fc
SSylvain1989 <sylvain.sigonnez@gmail.com>
2021-06-09 17:41:50 +0200

    feat: add tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,25 @@
-// like root in react
+// this file is like root in react
+import '../styles/globals.css'
 import Link from 'next/link';
 
 function MyApp({ Component, pageProps }) {
-  return(
-  <div>
-    <nav>
-      <ul>
-        <li>
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/about">
-            <a>About</a>
-          </Link>
-        </li>
-      </ul>
-    </nav>
-    <Component {...pageProps} />
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Component {...pageProps} />
     </div>
   );
 }
