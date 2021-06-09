b3add37cca04f365f120577936502a6f94e51d6f
SSylvain1989 <sylvain.sigonnez@gmail.com>
2021-06-09 22:32:50 +0200

    feat: add style to layout

--- pages/_app.js
+++ pages/_app.js
@@ -4,21 +4,24 @@ import Link from 'next/link';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/about">
-              <a>About</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
+    <div className="mx-auto w-9/12 my-8">
+      <header>
+          <h1 className="text-6xl font-bold text-center">My blog</h1>
+        <nav className="my-14">
+          <ul className="flex flex-row justify-center space-x-4">
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                <a>About</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
       <Component {...pageProps} />
     </div>
   );
